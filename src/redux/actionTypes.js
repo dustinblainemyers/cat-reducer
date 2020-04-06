2c6e7163da5600628833de469f7baa6d22422295
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-06 15:37:10 -0400

    Monday Cat reducer follow along complete

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,3 @@
+export const ACTION_NAP = "ACTION_NAP"
+export const ACTIN_EAT = "ACTION_EAT";
+export const ACTION_PLAY = "ACTION_PLAY";
---
+++ src/redux/actions.js
@@ -0,0 +1,25 @@
+import {ACTION_NAP, ACTION_EAT, ACTION_PLAY} from './actionTypes';
+
+
+export const nap = () => {
+    return {
+        type: ACITON_NAP
+
+
+    }
+}
+
+
+export const eat = () => {
+
+    return {
+        type: ACTION_EAT
+    }
+}
+
+
+export const play = () => {
+    return {
+        type: ACTION_PLAY
+    }
+}
